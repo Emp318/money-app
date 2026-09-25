1fdf95ab8a32d37c07da51c69c5b514ca0026db9
Olatunji <e.olatunji318@gmail.com>
2026-09-25 10:46:16 +0100

    backend-done

---
+++ src/main/kotlin/com/example/transaction_management/TransactionAccessDeniedException.kt
@@ -0,0 +1,4 @@
+package com.example.transaction_management
+
+class TransactionAccessDeniedException {
+}
--- src/main/kotlin/com/example/transaction_management/TransactionRepository.kt
+++ src/main/kotlin/com/example/transaction_management/TransactionRepository.kt
@@ -1,4 +1,7 @@
 package com.example.transaction_management
 import org.springframework.data.jpa.repository.JpaRepository
 
+interface TransactionRepository : JpaRepository<Transaction, Long> {
+    fun findByOwnerId(ownerId: Long): List<Transaction>
+}
 interface TransactionRepository : JpaRepository<Transaction,Long>
